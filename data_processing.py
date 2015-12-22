2f9b4a9e787a60b6b86ad82430afe2f8e2c5cfa8
kmlee17 <kmlee1717@yahoo.com>
2015-12-22 01:40:15 -0800

    Susie & Kevin fraud 12/21 update

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[23]:
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,23 +16,24 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 import datetime
 
-
-# In[4]:
+# Taken from the pickled random forest model feature importance
+top_features = ['object_id', 'CHECK', 'body_length', 'event_published', 'has_logo',
+                'user_created', 'venue_longitude', 'gts', 'GBP', 'org_twitter',
+                'org_facebook', 'num_order', 'sus_domain', 'user_age', 'sale_duration',
+                'sale_duration2', 'BLANK', 'delivery_method','user_type', 'len_pp']
 
 def convert_unix_timestamp(df, column_name):
     df[column_name] = df[column_name].map(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
 
 
-# In[2]:
-
 def add_dummies(df, column_name, baseline):
     dummies = pd.get_dummies(df[column_name])
+    if column_name == 'payout_type':
+        dummies.rename(columns={'': 'BLANK'}, inplace=True)
     dummies.drop(baseline, axis=1, inplace=True)
     df.drop(column_name, axis=1, inplace=True)
     return pd.concat((df, dummies), axis=1)
 
-
-# In[3]:
 
 def length_of_feature(df, column_name, length_col_name, drop_orig=True):
     df[length_col_name] = map(lambda x: len(x), df[column_name])
@@ -45,7 +41,6 @@ def length_of_feature(df, column_name, length_col_name, drop_orig=True):
         df.drop(column_name, axis=1, inplace=True)
 
 
-# In[5]:
 def fuzzy(x):
     return SM(None, x['org_name'], x['payee_name']).ratio()
 
@@ -92,8 +87,6 @@ def smote(X, y, minority_weight=.5):
     return X, y
 
 
-# In[6]:
-
 def feature_importance(model, dfX, dfy):
     fi = model.feature_importances_
     yticks = X.columns[np.argsort(fi)].values
@@ -106,11 +99,70 @@ def feature_importance(model, dfX, dfy):
 
 
 # ## Read in data
+def process_data(df): 
+    #convert_unix_timestamp(df, 'event_created')
+    #convert_unix_timestamp(df, 'event_start')
+    #convert_unix_timestamp(df, 'event_end')
+    # Filling empty values
+    #df['event_published'] = df['event_published'].fillna(1352831618)
+    #convert_unix_timestamp(df, 'event_published')
+
+    #convert_unix_timestamp(df, 'user_created')
+
+
+    # Countries dummies ('US' as baseline)
+    df = add_dummies(df, 'country', 'US')
+    # Currency dummies ('USD' as baseline)
+    df = add_dummies(df, 'currency', 'USD')
+    if 'GBP' not in df:
+        df['GBP'] = 0
+    # Payouts dummies ('ACH' as baseline)
+    df = add_dummies(df, 'payout_type', 'ACH')
+    if 'CHECK' not in df:
+        df['CHECK'] = 0
+    if 'BLANK' not in df:
+        df['BLANK'] = 0
+
+    #featurizing strings as 'length'
+    length_of_feature(df, 'description', 'len_desc')
+    length_of_feature(df, 'previous_payouts', 'len_pp')
+    length_of_feature(df, 'org_desc', 'len_org_desc')
+    length_of_feature(df, 'ticket_types', 'len_tt')
+
+    # imputting missing values
+    df['has_header'] = df['has_header'].fillna(2)
+    df['venue_latitude'] = df['venue_latitude'].fillna(0.)
+    df['venue_longitude'] = df['venue_longitude'].fillna(0.)
+    df['sale_duration'] = df['sale_duration'].fillna(df['sale_duration'].mean())
+    df['org_facebook'] = df['org_facebook'].fillna(0)
+    df['org_twitter'] = df['org_twitter'].fillna(0)
+    df['event_published'] = df['event_published'].fillna(0)
+    df['delivery_method'] = df['delivery_method'].fillna(0)
+
+    # making 'listed' legible
+    df['listed'] = df['listed'].map({'y': 1, 'n': 0})
+
+    # dropping less useful columns
+    df.drop(['venue_state', 'venue_name', 'venue_address', 'venue_country'], axis=1, inplace=True)
+
+    # feature engineering suspicious emails
+    sus_domains = ["gmail.com", "yahoo.com", "hotmail.com", "ymail.com","aol.com", \
+                   "lidf.co.uk"," live.com", "live.fr", "yahoo.co.uk", "rocketmail.com"]
+    df['sus_domain'] = map(lambda x: True if x in sus_domains else False, df['email_domain'])
+    df.drop(['email_domain'], axis=1, inplace=True)
+
+    # creating similiarity score for payee vs org name features
+    df['fuzzy_sim'] = df.apply(fuzzy, axis=1)
+
+    df.drop(['name', 'org_name', 'payee_name'], axis=1, inplace=True)
+
+    # Creating the top20 feature matrix
+    df_top = df[top_features]
+    return df_top
 
 # In[7]:
 def process_model(data): 
     df = pd.read_json(data)
-
 
     # Find rows that are fraudulent
     frauds = df['acct_type'].str.contains('fraudster')
@@ -130,8 +182,12 @@ def process_model(data):
     df = add_dummies(df, 'country', 'US')
     # Currency dummies ('USD' as baseline)
     df = add_dummies(df, 'currency', 'USD')
+    # Adds 'GBP' column if it wasn't created in add_dummies
+    
     # Payouts dummies ('ACH' as baseline)
     df = add_dummies(df, 'payout_type', 'ACH')
+
+    # df_payout.rename(columns={'': 'BLANK'}, inplace=True)
 
     #featurizing strings as 'length'
     length_of_feature(df, 'description', 'len_desc')
--- predict.py
+++ predict.py
@@ -1,72 +1,27 @@
-# python predict.py test_script_examples.csv
+# python predict.py example.json
 import sys
 import pandas as pd
-#from data_processing import add_dummies, length_of_feature, fuzzy
 import data_processing as dp
+import cPickle as pickle
+from pymongo import MongoClient
 
-def process_data(data): 
+
+def predict(data):
     df = pd.read_json(data)
-
-    #convert_unix_timestamp(df, 'event_created')
-    #convert_unix_timestamp(df, 'event_start')
-    #convert_unix_timestamp(df, 'event_end')
-    # Filling empty values
-    #df['event_published'] = df['event_published'].fillna(1352831618)
-    #convert_unix_timestamp(df, 'event_published')
-
-    #convert_unix_timestamp(df, 'user_created')
-
-
-    # Countries dummies ('US' as baseline)
-    df = dp.add_dummies(df, 'country', 'US')
-    # Currency dummies ('USD' as baseline)
-    df = dp.add_dummies(df, 'currency', 'USD')
-    # Payouts dummies ('ACH' as baseline)
-    df = dp.add_dummies(df, 'payout_type', 'ACH')
-
-    #featurizing strings as 'length'
-    dp.length_of_feature(df, 'description', 'len_desc')
-    dp.length_of_feature(df, 'previous_payouts', 'len_pp')
-    dp.length_of_feature(df, 'org_desc', 'len_org_desc')
-    dp.length_of_feature(df, 'ticket_types', 'len_tt')
-
-    # imputing missing values
-    df['has_header'] = df['has_header'].fillna(2)
-    df['venue_latitude'] = df['venue_latitude'].fillna(0.)
-    df['venue_longitude'] = df['venue_longitude'].fillna(0.)
-    df['sale_duration'] = df['sale_duration'].fillna(df['sale_duration'].mean())
-    df['org_facebook'] = df['org_facebook'].fillna(0)
-    df['org_twitter'] = df['org_twitter'].fillna(0)
-    df['event_published'] = df['event_published'].fillna(0)
-    df['delivery_method'] = df['delivery_method'].fillna(0)
-
-    # making 'listed' legible
-    df['listed'] = df['listed'].map({'y': 1, 'n': 0})
-
-    # dropping less useful columns
-    df.drop(['venue_state', 'venue_name', 'venue_address', 'venue_country'], axis=1, inplace=True)
-
-    # feature engineering suspicious emails
-    sus_domains = ["gmail.com", "yahoo.com", "hotmail.com", "ymail.com","aol.com", \
-                   "lidf.co.uk"," live.com", "live.fr", "yahoo.co.uk", "rocketmail.com"]
-    df['sus_domain'] = map(lambda x: True if x in sus_domains else False, df['email_domain'])
-    df.drop(['email_domain'], axis=1, inplace=True)
-
-    # creating similiarity score for payee vs org name features
-    df['fuzzy_sim'] = df.apply(dp.fuzzy, axis=1)
-
-    df.drop(['name', 'org_name', 'payee_name'], axis=1, inplace=True)
-
-
+    X_top = dp.process_data(df)
+    with open('data/random_forest.pkl') as f:
+        rf_top = pickle.load(f)
+    y_pred = rf_top.predict_proba(X_top)
+    df['fraud_prob'] = y_pred[:,1]
     return df
 
+def store_in_db(df):
+    client = MongoClient()
+    db = client['fraud_db']
+    tab = db['fraud']
+    tab.insert_many(df.to_dict('records'))
 
-def predict(X):
-	X_processed = process_data(X)
-	return X_processed
-	# unpack pickled model
-	# predict X_processed on pickled model
-	# return prediction
 
 if __name__ == '__main__':
-	test = predict(sys.argv[1])
+    df = predict(sys.argv[1])
+    store_in_db(df)
