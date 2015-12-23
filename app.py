c903339c8edb72e93a39e5d89d8f5bc5e5ca2815
kmlee17 <kmlee1717@yahoo.com>
2015-12-22 16:47:31 -0800

    final stuff

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import requests
 import socket
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tempfile
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,25 +18,49 @@ def index():
 	high = tab.find({'fraud_prob': {'$gt': 0.5}}).count()
 	medium = tab.find({'fraud_prob': {'$lte': 0.5, '$gt': 0.0}}).count()
 	low = tab.find({'fraud_prob': 0.0}).count()
-	return render_template('index.html', high_var = high, med_var = medium, low_var = low)
+	img = graph(high, medium, low)
+	return render_template('index.html', high_var = high, med_var = medium, low_var = low, img_1 = img)
+
+def graph(high, med, low): 
+    labels = 'high risk', 'medium risk', 'low risk'
+    sizes = [high, med, low]
+    colors = ['indianred', 'palegoldenrod', 'lightgreen']
+    explode = (0.1, 0, 0)
+
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors, shadow=True)
+
+
+    f = tempfile.NamedTemporaryFile(dir='static', suffix='.png',delete=False)
+    # save the figure to the temporary file
+    plt.savefig(f)
+    f.close() # close the file
+    # get the file's name (rather than the whole path)
+    # (the template will need that)
+    plotPng = f.name.split('/')[-1]
+    return plotPng
+
 
 @app.route('/high_risk')
 def high_risk():
 	high = tab.find({'fraud_prob': {'$gt': 0.5}})
 	df = pd.DataFrame(list(high))
-	return df.to_html()
+	return '<link href="static/css/css_table.css" rel="stylesheet">' + df.to_html(classes="CSSTableGenerator")
+	# with "high_risk.html" open as f:
+	# 	f.write(html_df)
+	#return '<link href="static/css/css_table.css" rel="stylesheet"><div class="CSSTableGenerator">' + df.to_html(classes="") + '</div>'
 
 @app.route('/med_risk')
 def med_risk():
 	medium = tab.find({'fraud_prob': {'$lte': 0.5, '$gt': 0.0}})
 	df = pd.DataFrame(list(medium))
-	return df.to_html()
+	return '<link href="static/css/css_table.css" rel="stylesheet">' + df.to_html(classes="CSSTableGenerator")
+
 
 @app.route('/low_risk')
 def low_risk():
 	low = tab.find({'fraud_prob': 0.0})
 	df = pd.DataFrame(list(low))
-	return df.to_html()
+	return '<link href="static/css/css_table.css" rel="stylesheet">' + df.to_html(classes="CSSTableGenerator")
 
 
 
