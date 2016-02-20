bce2252cd8c950302f34314eb11000ceaceda9ab
susiexsun <susiexsun@gmail.com>
2016-02-19 17:38:55 -0800

    Delete post_request.py

--- dashboard/post_request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-with open('example.json', 'rb') as f:
-	data = f.read()
-#     r = requests.post("http://0.0.0.0:8080/score", files={'example.json': f})
-
-r = requests.post("http://0.0.0.0:8080/score", json=data)
